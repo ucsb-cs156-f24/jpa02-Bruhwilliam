c4ae534087420e0a33c93f5312ed0e061bd8c4ca
Bruhwilliam <will1am.lau5264@gmail.com>
2024-10-15 17:11:02 -0700

    added tests

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -19,8 +19,61 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
 
+    @Test
+    public void equals_sameObject_returnsTrue() {
+        assertEquals(true, team.equals(team));
+    }
+
+    @Test
+    public void equals_withNull_returnsFalse() {
+        assertEquals(false, team.equals(null));
+    }
+
+    @Test
+    public void equals_differentClass_returnsFalse() {
+        Object differentClassObject = new Object();
+        assertEquals(false, team.equals(differentClassObject));
+    }
+    @Test
+    public void equals_identicalTeams_returnsTrue() {
+        Team anotherTeam = new Team("test-team");
+        assertEquals(true, team.equals(anotherTeam));
+    }
+
+    @Test
+    public void equals_teamsWithSameNameDifferentMembers_returnsFalse() {
+        Team otherTeam = new Team("test-team");
+        team.addMember("Alice");
+        otherTeam.addMember("Bob");
+        assertEquals(false, team.equals(otherTeam));
+    }
+
+    @Test
+    public void equals_teamsWithDifferentNamesSameMembers_returnsFalse() {
+        Team otherTeam = new Team("another-team");
+        team.addMember("Alice");
+        otherTeam.addMember("Alice");
+        assertEquals(false, team.equals(otherTeam));
+    }
+    
+
+    @Test
+    public void hashCode_equalObjectsHaveSameHashCode() {
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("bar");
+
+        Team t2 = new Team();
+        t2.setName("foo");
+        t2.addMember("bar");
+
+        assertEquals(t1.hashCode(), t2.hashCode(), "Hash codes should be equal for identical teams");
+    }
 }
